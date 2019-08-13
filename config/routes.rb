ddc01ec037623fccedc0778ac8dc6e2c29fcf48d
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-08-13 08:45:32 -0600

    Fixing routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   root 'products#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index]
+  get '/products/:path', to: 'products#show', as: 'product'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      resources :orders, only: [:create]
+      post '/update-user', to: 'users#update'
+    end
+  end
+
+  get '/login' => 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/:slug', to: 'users#show', as: 'user'
 end
